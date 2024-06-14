d8337c17559d7dca0a65a7b1be5537ed43456cdb
B30419 <6534qgas1793@gmail.com>
2024-06-14 14:51:12 +0800

    init

---
+++ test.py
@@ -0,0 +1,25 @@
+def gcd(a, b):
+    if a % b == 0:
+        return b
+    return gcd(b, a % b)
+
+
+def fab(a):
+    if a == 0 or a == 1:
+        return 1
+    return fab(a - 1) + fab(a - 2)
+
+
+def fab_loop(n):
+    a = 1
+    b = 1
+    for i in range(1, n):
+        res = a + b
+        a = b
+        b = res
+    return res
+
+
+print(gcd(6, 100))
+print(fab(10))
+print(fab_loop(10))
